406296adab7c8dbae235ea97500a5eea1954f62c
Noelia Figueiras Fernández <noelia.figueiras@vigo.org>
2021-11-25 12:18:48 +0100

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -9,3 +9,14 @@ email.addEventListener("input", function (event) {
     email.setCustomValidity("");
   }
 });
+
+// tendría que hacer algo parecido para los patterns de name y phone
+const name = document.getElementById("name");
+
+name.addEventListener("input", function (event) {
+  if (name.validity.typeMismatch) {
+    name.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+  } else {
+    name.setCustomValidity("");
+  }
+});
